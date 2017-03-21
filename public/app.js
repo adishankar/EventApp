46dd464b1a2b867f3b5cf69d9a61a7bd05bd419c
adishankar <adi2895@gmail.com>
2017-03-21 18:16:52 -0400

    reorganization

--- public/app.js
+++ public/app.js
@@ -2,4 +2,14 @@
 angular.module('app', [])
     .controller('AppCtrl', ['$scope', function AppCtrl($scope) {
         $scope.hello = "hello world";
+
+        $scope.signUp = function(){
+            //redirect to signup page
+        }
+
+        $scope.logIn = function(){
+
+        }
+
+
     }]);
---
+++ public/signup/signup.js
@@ -0,0 +1,6 @@
+
+angular.module('signup', [])
+    .controller('SignUpCtrl', ['$scope', function SignUpCtrl($scope) {
+        $scope.hello = "hello world";
+
+    }]);
