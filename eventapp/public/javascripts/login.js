837769ed86aa6cbe5a2b084e516cf3b1bddde62f
adishankar <adi2895@gmail.com>
2017-03-21 19:28:54 -0400

    set index.html as start page

---
+++ eventapp/public/javascripts/login.js
@@ -0,0 +1,4 @@
+angular.module('app', [])
+     .controller('AppCtrl', ['$scope', function AppCtrl($scope) {
+         $scope.hello = "hello world";
+     }]);
--- eventapp/routes/index.js
+++ eventapp/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. 
+router.get('/index.html', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.sendFile('/views/index.html');
+});*/
+
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('/home/adi/nodestuff/databases/EventApp/eventapp/views/index.html');
 });
 
 module.exports = router;
