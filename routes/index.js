f642fd35c8294153c9d5ce180053e0821e9e0494
adishankar <adi2895@gmail.com>
2017-03-21 19:02:32 -0400

    express generator

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();//declare router object
-
-router.get('/', function(req, res){
-
-    res.sendFile(__dirname + '/public/dashboard/dashboard.html');
-});
-
-module.exports = router;
